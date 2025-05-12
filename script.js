ba235eafc92e455b5da10a7d26c17e7b637434c7
Divya182003 <divyabhootda05@gmail.com>
2025-05-12 10:25:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+//function firstNonRepeatedChar(str) {
+ //let count0;
+	//let length=str.length;
+	//for i in str:
+	//for j in
+	 //count=count+1;
 function firstNonRepeatedChar(str) {
- // Write your code here
+  const charCount = {};
+  for (let char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+
+  return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+
+
